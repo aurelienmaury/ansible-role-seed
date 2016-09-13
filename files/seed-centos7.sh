0f1c2212450838d5408f82ba4d454e5fd28334bf
A. Maury <aurelien.maury@wescale.fr>
2016-09-13 17:26:23 +0200

    init

--- files/seed-centos7.sh
+++ files/seed-centos7.sh
@@ -3,36 +3,35 @@
 set -e
 set -x
 
-if [ ! -d "/etc/ansible" ]; then
-    yum update -y
-
-    yum install \
-      python-setuptools \
-      python-devel \
-      openssl-devel \
-      sudo \
-      libffi-devel \
-      git \
-      gcc \
-      gcc-c++ \
-      make \
-      openssl-devel \
-      libxml2 \
-      libxml2-devel \
-      libxslt \
-      libxslt-devel \
-      perl-devel \
-      automake \
-      -y
-
-    echo "Installing pip via easy_install"
-    for i in 1 2 3 4 5; do easy_install pip && break || sleep 2; done
-
-    echo "Installing correct python crypto libs"
-    pip install -U pyopenssl ndg-httpsclient pyasn1
-
-    echo "Installing ansible via pip"
-    pip install -U pip ansible
-
-    mkdir -p /etc/ansible/
+if [ -d "/etc/ansible" ]; then
+    exit 42
 fi
+
+yum update -y
+
+yum install \
+  python-setuptools \
+  python-devel \
+  openssl-devel \
+  sudo \
+  libffi-devel \
+  git \
+  gcc \
+  gcc-c++ \
+  make \
+  openssl-devel \
+  libxml2 \
+  libxml2-devel \
+  libxslt \
+  libxslt-devel \
+  perl-devel \
+  automake \
+  -y
+
+for i in 1 2 3 4 5; do easy_install pip && break || sleep 2; done
+
+pip install -U pyopenssl ndg-httpsclient pyasn1
+
+pip install -U pip ansible
+
+mkdir -p /etc/ansible/
--- files/seed-debian.sh
+++ files/seed-debian.sh
@@ -3,39 +3,42 @@
 set -e
 set -x
 
-if [ ! -d "/etc/ansible" ]; then
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update
-
-    if [ -n "$(dpkg-query -l python-pip 2> /dev/null)" ]; then
-        apt-get -o Dpkg::Options::="--force-confold" remove python-pip --purge -y
-    fi
-
-    apt-get -o Dpkg::Options::="--force-confold" upgrade -y
-
-    apt-get update
-
-    apt-get -o Dpkg::Options::="--force-confold" install \
-        sudo                \
-        aptitude            \
-        git                 \
-        python-apt          \
-        apt-transport-https \
-        python-distutils-extra \
-        python-setuptools   \
-        python-apt-dev      \
-        python-dev          \
-        libssl-dev          \
-        libffi-dev          \
-        build-essential     \
-        libperl-dev         \
-        -y
-
-    for i in 1 2 3 4 5; do easy_install pip && break || sleep 2; done
-
-    pip install -U pyopenssl ndg-httpsclient pyasn1
-
-    pip install -U pip ansible
-
-    mkdir -p /etc/ansible/
+if [ -d "/etc/ansible" ]; then
+    exit 42
 fi
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+
+if [ -n "$(dpkg-query -l python-pip 2> /dev/null)" ]; then
+    apt-get -o Dpkg::Options::="--force-confold" remove python-pip --purge -y
+fi
+
+apt-get -o Dpkg::Options::="--force-confold" upgrade -y
+
+apt-get update
+
+apt-get -o Dpkg::Options::="--force-confold" install \
+    sudo                \
+    aptitude            \
+    git                 \
+    python-apt          \
+    apt-transport-https \
+    python-distutils-extra \
+    python-setuptools   \
+    python-apt-dev      \
+    python-dev          \
+    libssl-dev          \
+    libffi-dev          \
+    build-essential     \
+    libperl-dev         \
+    -y
+
+for i in 1 2 3 4 5; do easy_install pip && break || sleep 2; done
+
+pip install -U pyopenssl ndg-httpsclient pyasn1
+
+pip install -U pip ansible
+
+mkdir -p /etc/ansible/
