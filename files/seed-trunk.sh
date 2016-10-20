2b8bfacc5e5a196274a2c498230dda8fefd4f22e
A. Maury <aurelien.maury@wescale.fr>
2016-10-20 14:17:28 +0200

    trunk hardening

--- files/seed-trunk.sh
+++ files/seed-trunk.sh
@@ -3,10 +3,13 @@
 set -e
 set -x
 
+FM_CONF_FILE="/etc/yum/pluginconf.d/fastestmirror.conf"
+FM_TIMED_HOSTS="/var/cache/yum/timedhosts.txt"
+
 function yum_based_lsb_install {
     which yum \
-    && sed -i -e 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf  \
-    && rm -f /var/cache/yum/timedhosts.txt \
+    && ( [ ! -e "$FM_CONF_FILE" ]   || sed -i -e 's/enabled=1/enabled=0/' $FM_CONF_FILE ) \
+    && ( [ ! -e "$FM_TIMED_HOSTS" ] || rm -f $FM_TIMED_HOSTS ) \
     && sleep 2 \
     && yum makecache -y \
     && yum install redhat-lsb-core -y
